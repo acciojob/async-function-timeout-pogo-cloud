639b6feb6a7f469b0049bc743ec8a48073f92944
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2025-01-01 19:49:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+
+let but=document.querySelector("#btn");
+let output=document.getElementById("output");
+btn.addEventListener("click",(event)=>{
+	event.preventDefault();
+	ted();
+})
+const display=()=>{
+let ele=document.querySelector("#text");
+let num=document.querySelector("#num");
+	return Promise((res)=>{
+		if(ele.value!==" "&&num.value!==" "){
+		   res(output.innerHTML=ele.value+"-"+num.value);
+		}
+	})
+}
+async function ted(){
+    setTimeOut(()=>{
+		const p= await display();
+	},1000)
+}
